d66292ade47cc55d69abe819d2ac00dd9d426799
Marcos Gabriel <marcospessoalmp@outlook.com.br>
2022-08-02 21:01:25 -0300

    first commit

---
+++ buildar/src/main/java/com/example/buildar/BuildarApplication.java
@@ -0,0 +1,14 @@
+package com.example.buildar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BuildarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildarApplication.class, args);
+	}
+
+}
---
+++ buildar/src/test/java/com/example/buildar/BuildarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
